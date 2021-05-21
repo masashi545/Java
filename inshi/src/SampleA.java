a5fc56e5a7d1223c8f44eab2d5f87281fb66d57c
masashi545 <38308946+masashi545@users.noreply.github.com>
2021-05-21 13:12:07 +0900

    Create inshi

---
+++ inshi/src/SampleA.java
@@ -0,0 +1,9 @@
+class SampleA {
+    public static void main(String[] args) {
+        Poly p1 = new Poly();
+
+        double sum = p1.culculation(1.0);
+
+        System.out.println(sum);
+    }
+}
